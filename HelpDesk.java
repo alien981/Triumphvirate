35f0f1150cca3e2cbfc530477f2d154fecc396c6
alien981 <dsparakis@stuy.edu>
2017-04-20 23:32:47 -0400

    added methods to HelpDesk, APQ orders least to most important, not most to least

--- HelpDesk.java
+++ HelpDesk.java
@@ -1,18 +1,62 @@
 public class HelpDesk{
 
-	private String[] services;
+	private String[] services = {"0: Send someone to deal with this", "1: My computer crashed", "2: How do you install Java?", "3: I forgot my password", "4: I clicked something", "5: IDK" , "101: I want pizza"};;
 	private ArrayPriorityQueue queue;
 	private int nextTicket;
 	
 	public HelpDesk(){
-		services = {"0: Send someone to deal with this", "1: My computer crashed", "2: How do you install Java?", "3: I forgot my password", "4: I clicked something", "5: IDK" , "101: I want pizza"};
+		nextTicket = 0;
+		queue = new ArrayPriorityQueue();
 	}
-	
 	
 	public void submitTicket(String yourName, String problem, int serviceNum){
-		Ticket q = newTicket(yourName, problem, nextTicket, serviceNum);
+		Ticket q = new Ticket(yourName, problem, nextTicket, serviceNum);
 		nextTicket++;
+		queue.add(q);
+	}
+	
+	public String toString(){
+		return queue.toString();
+	}
+	
+/*		
+	
+
+	
+	public String resolveIssue(){
+		String q = "Issue solved: ";
+		q += queue.removeMin();
+		return q;
 	}
 
+	public String getQueue(){
+		String w = "";
+		for(Ticket q: queue){
+			w+= q + ", ";
+		}
+		return w;
+	}
 
+	public String getServices(){
+		String q = "";
+		for(String w: services){
+			q+= w + "/n";
+		}
+		return q;
+	}
+	
+	*/
+	
+	public static void main(String[] args){
+		HelpDesk q = new HelpDesk();
+		q.submitTicket("q", "f", 5);
+		q.submitTicket("q", "a", 0);
+		q.submitTicket("q", "b", 2);
+		q.submitTicket("q", "c", 2);
+		q.submitTicket("q", "e", 4);
+		q.submitTicket("q", "d", 3);
+		System.out.println(q);
+		
+	}
+	
 }
