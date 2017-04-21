d640f1832172f2432fba57ecca6057fbea2169f6
kristinlin <klin11@stuy.edu>
2017-04-21 08:35:38 -0400

    implements Comparable

--- Ticket.java
+++ Ticket.java
@@ -1,33 +1,37 @@
-public class Ticket{
+public class Ticket implements Comparable{
 
-	private int priority;
-	private String description;
-	private String typeOfProblem;
-	private int number;
-	private String name;
-	private String solution;
-	private boolean resolved;
+    private int priority;
+    private String description;
+    private String typeOfProblem;
+    private int number;
+    private String name;
+    private String solution;
+    private boolean resolved;
 	
-	public Ticket(int num, String cargo){
-		priority = num;
-		description = cargo;
-		resolved = false;
-	}
+    public Ticket(int num, String cargo){
+	priority = num;
+	description = cargo;
+	resolved = false;
+    }
 	
-	public Ticket(String yourName, String desc, int num, int serviceNum){
-		description = desc;
-		name = yourName;
-		number = num;
-		priority = serviceNum;
-	}
+    public Ticket(String yourName, String desc, int num, int serviceNum){
+	description = desc;
+	name = yourName;
+	number = num;
+	priority = serviceNum;
+    }
 	
-	public int getPriority(){
-		return priority;
-	}
+    public int getPriority(){
+	return priority;
+    }
+
+    public int compareTo(Object x) {
+	return priority - ( (Ticket) x).getPriority();
+    }
 	
-	public String toString(){
-		return description;
-	}
+    public String toString(){
+	return description;
+    }
 
 
 
