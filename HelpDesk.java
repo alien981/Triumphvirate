e240cc0975d5463d9144925dd99d5bb7178faaaf
alien981 <dsparakis@stuy.edu>
2017-04-20 22:58:47 -0400

    worked on constructors

--- HelpDesk.java
+++ HelpDesk.java
@@ -1,5 +1,18 @@
 public class HelpDesk{
 
+	private String[] services;
+	private ArrayPriorityQueue queue;
+	private int nextTicket;
+	
+	public HelpDesk(){
+		services = {"0: Send someone to deal with this", "1: My computer crashed", "2: How do you install Java?", "3: I forgot my password", "4: I clicked something", "5: IDK" , "101: I want pizza"};
+	}
+	
+	
+	public void submitTicket(String yourName, String problem, int serviceNum){
+		Ticket q = newTicket(yourName, problem, nextTicket, serviceNum);
+		nextTicket++;
+	}
 
 
 }
--- Ticket.java
+++ Ticket.java
@@ -1,5 +1,21 @@
 public class Ticket{
 
+	private int priority;
+	private String description;
+	private String typeOfProblem;
+	private int number;
+	private String name;
+	
+	public Ticket(String yourName, String desc, int num, int serviceNum){
+		description = desc;
+		name = yourName;
+		number = num;
+		priority = serviceNum;
+	}
+	
+	public int getPriority(){
+		return priority;
+	}
 
 
 
