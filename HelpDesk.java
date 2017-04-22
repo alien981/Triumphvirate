f896902d5837edab54a0e9904cd6f2d5b088fd32
chan lin <klin11@stuy.edu>
2017-04-22 16:23:01 -0400

    added classify()

--- HelpDesk.java
+++ HelpDesk.java
@@ -1,60 +1,87 @@
 public class HelpDesk{
 
-	private String[] services = {"0: Send someone to deal with this", "1: My computer crashed", "2: How do you install Java?", "3: I forgot my password", "4: I clicked something", "5: IDK" , "101: I want pizza"};
-	private ArrayPriorityQueue queue;
-	private int nextTicket;
+    private String[] services = {"0: Send someone to deal with this", "1: My computer crashed", "2: How do you install Java?", "3: I forgot my password", "4: I clicked something", "5: IDK" , "101: I want pizza"};
+    private ArrayPriorityQueue queue;
+    private int nextTicket;
 	
-	public HelpDesk(){
-		nextTicket = 0;
-		queue = new ArrayPriorityQueue();
-	}
+    public HelpDesk(){
+	nextTicket = 0;
+	queue = new ArrayPriorityQueue();
+    }
 	
-	public void submitTicket(String yourName, String problem, int serviceNum){
-		Ticket q = new Ticket(yourName, problem, nextTicket, serviceNum);
-		nextTicket++;
-		queue.add(q);
+    public void submitTicket(String yourName) {
+	serviceNum = classify();
+	Ticket q = new Ticket(yourName, problem, nextTicket, serviceNum);
+	nextTicket++;
+	queue.add(q);
+    }
+
+    //to determine the priority of issuee
+    public int classify() {
+	System.out.println("Is your computer booting up? \n1: no \n2: yes");
+	if ("insert code" == 1) {
+	    return 1;
+	} else {
+	    System.out.println("Is your screen blank? \n1: no \n2: yes");
+		if ("insertcode" == 2) {
+		    return 2;
+		} else {
+		    System.out.println("Is your computer having internet issues? \n1: no \n2: yes");
+			if ("insert code" == 2) {
+			    return 3;
+			} else {
+			    System.out.println("Is your computer slow or frozen? \n1: no \n2: yes" )
+				if ("insert code" == 2) {
+				    return 4;
+				} else {
+				    System.out.println("Thank you for your response."
+						       return 5;
+
+
+			}
 	}
+    }
 	
-	public String toString(){
-		return queue.toString();
-	}
+    public String toString(){
+	return queue.toString();
+    }
 	
 
-	public String resolveIssue(){
-		String q = "Issue solved: ";
-		q += queue.removeMin();
-		return q;
-	}
+    public String resolveIssue(){
+	String q = "Issue solved: ";
+	q += queue.removeMin();
+	return q;
+    }
 
 
-	public String getServices(){
-		String q = "";
-		for(String w: services){
-			q+= w + "\n";
-		}
-		return q;
+    public String getServices(){
+	String q = "";
+	for(String w: services){
+	    q+= w + "\n";
 	}
+	return q;
+    }
 	
 	
 	
-	public static void main(String[] args){
-		HelpDesk q = new HelpDesk();
-		q.submitTicket("q", "f", 5);
-		q.submitTicket("q", "a", 0);
-		q.submitTicket("q", "b", 2);
-		q.submitTicket("q", "c", 2);
-		q.submitTicket("q", "e", 4);
-		q.submitTicket("q", "d", 3);
-		System.out.println(q);
+    public static void main(String[] args){
+	HelpDesk q = new HelpDesk();
+	q.submitTicket("q", "f", 5);
+	q.submitTicket("q", "a", 0);
+	q.submitTicket("q", "b", 2);
+	q.submitTicket("q", "c", 2);
+	q.submitTicket("q", "e", 4);
+	q.submitTicket("q", "d", 3);
+	System.out.println(q);
 		
-		System.out.println(q.resolveIssue());
-		System.out.println(q.resolveIssue());
-		System.out.println(q.resolveIssue());
-		System.out.println(q.resolveIssue());
-		System.out.println(q.resolveIssue());
-		System.out.println(q.resolveIssue());
+	System.out.println(q.resolveIssue());
+	System.out.println(q.resolveIssue());
+	System.out.println(q.resolveIssue());
+	System.out.println(q.resolveIssue());
+	System.out.println(q.resolveIssue());
+	System.out.println(q.resolveIssue());
 		
-		System.out.println(q.getServices());
-	}
+	System.out.println(q.getServices());
+    }
 	
 }
