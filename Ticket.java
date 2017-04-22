f9005086826329f9b4bea90b86dcbf54ad126734
chan lin <klin11@stuy.edu>
2017-04-22 15:32:49 -0400

    added set methods

--- Ticket.java
+++ Ticket.java
@@ -28,6 +28,22 @@ public class Ticket implements Comparable{
 	return priority;
     }
 
+    public int setPriority(int p) {
+	priority = p;
+	return priority;
+    }
+
+    public String setType(String type) {
+	typeOfProblem = type;
+	return typeOfProblem;
+    }
+
+    public String setSolution(String sol) {
+	solution = sol;
+	resolved = true;
+	return solution;
+    }
+
     public int compareTo(Object x) {
 	return priority - ( (Ticket) x).getPriority();
     }
